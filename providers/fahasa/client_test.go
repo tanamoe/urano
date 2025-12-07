dbe25388e311b9260737114b61e2ce8498a03847
Nguyen Do <khoanguyen.do@outlook.com>
2025-12-07 20:40:14 +0700

    feat: implement fahasa provider

---
+++ providers/fahasa/client_test.go
@@ -0,0 +1,33 @@
+package fahasa
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProduct(t *testing.T) {
+	assert := assert.New(t)
+
+	rawJSON, err := os.ReadFile("../../test/data/fahasa_product.json")
+	assert.NoError(err)
+
+	var expected Product
+	err = json.Unmarshal(rawJSON, &expected)
+	assert.NoError(err)
+
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write(rawJSON)
+	}))
+
+	client := NewClient(WithDomain(srv.URL))
+
+	product, err := client.Product(t.Context(), 1)
+	assert.NoError(err)
+	assert.NotNil(product)
+	assert.Equal(&expected, product)
+}
