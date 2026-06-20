725980957809462ab609f0d0f22df0a6c493b485
Nguyen Do <khoanguyen.do@outlook.com>
2026-06-20 15:51:53 +0700

    fix: simplify migration

--- db/migrations/migrations.go
+++ db/migrations/migrations.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"embed"
-	"log/slog"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/jackc/pgx/v5/stdlib"
@@ -14,37 +13,35 @@ import (
 //go:embed *.sql
 var migrations embed.FS
 
-type Migration struct {
+type Migration interface {
+	Run(ctx context.Context) error
+}
+
+type migration struct {
 	db *sql.DB
 }
 
-func NewMigrationFromPool(dbPool *pgxpool.Pool) *Migration {
-	db := stdlib.OpenDBFromPool(dbPool)
-
-	return &Migration{
-		db: db,
-	}
+func NewMigration(db *sql.DB) Migration {
+	return &migration{db}
 }
 
-func (m *Migration) Close() error {
-	return m.db.Close()
+func NewMigrationFromPool(dbPool *pgxpool.Pool) Migration {
+	db := stdlib.OpenDBFromPool(dbPool)
+
+	return &migration{db: db}
 }
 
 // runs migration if present
-func (m *Migration) RunMigrations(ctx context.Context) error {
+func (m *migration) Run(ctx context.Context) error {
 	goose.SetBaseFS(migrations)
 
 	if err := goose.SetDialect("postgres"); err != nil {
 		return err
 	}
 
-	slog.InfoContext(ctx, "Running migrations...")
-
 	if err := goose.UpContext(ctx, m.db, "."); err != nil {
 		return err
 	}
 
-	slog.InfoContext(ctx, "Migration completed!")
-
-	return nil
+	return m.db.Close()
 }
